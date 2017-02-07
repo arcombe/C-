4520e8b95a85d656eda7a9b5606ce9b084f0f5e2
Arcombe <dat13aar@dtudent.lu.se>
2017-02-07 14:56:50 +0100

    ny match

--- lab2-A-DoneDone/word.cc
+++ lab2-A-DoneDone/word.cc
@@ -16,15 +16,32 @@ string Word::get_word() const {
 
 unsigned int Word::get_matches(const vector<string>& t) const {
 	unsigned int count = 0;
-	int diff;
-	for(string trigram : trigrams){
-		for(string other_trigram : t){
-			diff = trigram.compare(other_trigram);
-			if (diff <= 0){
-				if (diff == 0) count += 1;
-				break;
-			}
+	int i = 0, j = 0, diff;
+
+	while(i != static_cast<int>(trigrams.size()) && j != static_cast<int>(t.size())){
+		diff = trigrams[i].compare(t[j]);
+		if (diff < 0) i++;
+		else if (diff > 0) j++;
+		else {
+			count += 1;
+			i++;
 		}
 	}
+
 	return count;
 }
+
+// unsigned int Word::get_matches(const vector<string>& t) const {
+// 	unsigned int count = 0;
+// 	int diff;
+// 	for(string trigram : trigrams){
+// 		for(string other_trigram : t){
+// 			diff = trigram.compare(other_trigram);
+// 			if (diff <= 0){
+// 				if (diff == 0) count += 1;
+// 				break;
+// 			}
+// 		}
+// 	}
+// 	return count;
+// }
